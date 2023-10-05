bbd1662c1a99c70fcf69dff7bff97effa59d6230
Saipriya Thirvakadu <saipriyabalu@yahoo.co.in>
2023-10-05 12:28:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require("path");
 const app = express()
 port = 3000
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello World from Node.js!</h1>')
+    //res.send('<h1>Hello World from Node.js!</h1>')
+    res.sendFile(path.join(__dirname, "index.html"));
+
 })
 
 app.listen(port, function() {
