27354f730bda73e619405626435dae06efae86fa
Saipriya Thirvakadu <oracleidentitycloudservice/saipriya.thirvakadu@oracle.com>
2023-12-19 19:00:41 -0600

    updated app.js and index files

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 // index.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
-
 // Set up a route to serve the HTML file
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index1.html');
+  res.sendFile(path.join(__dirname, "index1.html"));
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log('Hello World app listening on port ' + port);
 });
