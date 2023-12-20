715ffff80ace110ee189fa961f33fbe72851a975
Saipriya Thirvakadu <saipriyabalu@yahoo.co.in>
2023-12-19 18:46:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 // Set up a route to serve the HTML file
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index1.html');
 });
 
 // Start the server
